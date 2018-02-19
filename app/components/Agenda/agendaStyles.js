8955954b0fde8ce0b69c3936d84d2c070e78a8e7
Derek Young <dthomasy@gmail.com>
2018-02-18 22:06:45 -0700

    Bugfix: cannot scroll to bottom of schedule/adgenda views

--- app/components/Agenda/agendaStyles.js
+++ app/components/Agenda/agendaStyles.js
@@ -2,7 +2,7 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   'scroll-view': {
-    paddingBottom: 60
+    paddingBottom: 90 + 90 + 10 // Header height + (dial diameter + dial padding) + padding over dial
   },
   empty: {
     display: 'flex',
